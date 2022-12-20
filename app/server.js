4801590eb61b6d1afad999e222248b5e001060d0
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-20 02:53:34 -0400

    Added: db connection and setup models

--- app/server.js
+++ app/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const db = require("./models");
+const Role = db.role;
 
 const app = express();
 
@@ -15,6 +17,28 @@ app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log('Drop and Resync Db');
+    initial();
+});
+
+function initial() {
+    Role.create({
+        id: 1,
+        name: "user"
+    });
+
+    Role.create({
+        id: 2,
+        name: "moderator"
+    });
+
+    Role.create({
+        id: 3,
+        name: "admin"
+    });
+}
 
 // simple route
 app.get("/", (req, res) => {
