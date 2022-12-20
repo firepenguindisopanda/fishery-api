518f6b6c9038e8e94e3a63fad0d8c981f5a4c4d0
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-20 11:40:43 -0400

    Added relationship for user and fish with bridge table fish caught

--- app/models/index.js
+++ app/models/index.js
@@ -26,7 +26,10 @@ db.sequelize = sequelize;
 
 db.user = require("../models/user.model.js")(sequelize, Sequelize);
 db.role = require("../models/role.model.js")(sequelize, Sequelize);
+db.fish = require("../models/fish.model.js")(sequelize, Sequelize);
+db.fishcaught = require("../models/fishcaught.model.js")(sequelize, Sequelize);
 
+// start of user and roles relationship
 db.role.belongsToMany(db.user, {
   through: "user_roles",
   foreignKey: "roleId",
@@ -37,6 +40,23 @@ db.user.belongsToMany(db.role, {
   foreignKey: "userId",
   otherKey: "roleId"
 });
+// end of user and roles relationship
+
+// start of fish and fishcaught relationship
+db.fish.belongsToMany(db.user, {
+    through: db.fishcaught,
+    foreignKey: "fishid",
+    otherKey: "userid"
+});
+// end of fish and fishcaught relationship
+
+// start of user and fishcaught relationship
+db.user.belongsToMany(db.fish, {
+    through: db.fishcaught,
+    foreignKey: "userid",
+    otherKey: "fishid"
+});
+// end of user and fishcaught relationship
 
 db.ROLES = ["user", "admin", "moderator"];
 
