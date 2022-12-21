cf6acebaaeaa81d04a968b52719cd2fbacc2709d
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-21 13:22:10 -0400

    Added: signup function for new users

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -5,3 +5,40 @@ const Role = db.role;
 const Op = db.Sequelize.Op;
 let jwt = require("jsonwebtoken");
 let bcrypt = require("bcryptjs");
+
+exports.signup = (req, res) => {
+    /**
+     * Save User to database
+     * - first hash the password from the request
+     * - then create a new user with the hashed password
+     * - then using the array of roles passed in the request, find the roles in the database
+     * - then assign the roles to the user Object created.
+     * - then save the user to the database
+     */
+    let hashedpwd = bcrypt.hashSync(req.body.password, 8);
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedpwd
+    }).then(user => {
+        if(req.body.roles){
+            Role.findAll({
+                where: {
+                    name: {
+                        [Op.or]: req.body.roles
+                    }
+                }
+            }).then(roles => {
+                user.setRoles(roles).then(() => {
+                    res.send({message: "User registered successfully!"});
+                });
+            });
+        }else{
+            user.setRoles([1]).then(() => {
+                res.send({message: "User registered successfully!"});
+            });
+        }
+    }).catch(err => {
+        res.status(500).send({message: err.message});
+    });
+};
