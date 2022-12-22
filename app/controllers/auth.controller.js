7e14aa6ac0b4e2cfaa83460284f6ea9ce3553dde
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-22 19:52:56 -0400

    Added: signin logic

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -42,3 +42,36 @@ exports.signup = (req, res) => {
         res.status(500).send({message: err.message});
     });
 };
+exports.signin = (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    }).then(user => {
+        if(!user){
+            return res.status(404).send({message: "User not found!"});
+        }
+        let passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+        if(!passwordIsValid){
+            return res.status(401).send({accessToken: null, message: "Invalid Password!"});
+        }
+        let token = jwt.sign({id: user.id}, config.secret, {
+            expiresIn: 86400 //24 hours
+        });
+        let authorities = [];
+        user.getRoles().then(roles => {
+            for(let i = 0; i < roles.length; i++){
+                authorities.push("ROLE_" + roles[i].name.toUpperCase());
+            }
+            res.status(200).send({
+                id: user.id,
+                username: user.username,
+                email: user.email,
+                roles: authorities,
+                accessToken: token
+            });
+        });
+    }).catch(err => {
+        res.status(500).send({message: err.message});
+    });
+};
