6c4182af8e1978c02a222958bcce2c42e4a66d3b
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-27 22:39:14 -0400

    Updated: user and refresh token association

--- app/models/index.js
+++ app/models/index.js
@@ -25,10 +25,25 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.user = require("../models/user.model.js")(sequelize, Sequelize);
+db.refreshToken = require("../models/refreshToken.model.js")(sequelize, Sequelize);
 db.role = require("../models/role.model.js")(sequelize, Sequelize);
 db.fish = require("../models/fish.model.js")(sequelize, Sequelize);
 db.fishcaught = require("../models/fishcaught.model.js")(sequelize, Sequelize);
 db.sold = require("../models/sold.model.js")(sequelize, Sequelize);
+
+// start of user and refresh token relationship
+db.refreshToken.belongsTo(db.user, {
+  foreignKey: 'userId',
+  targetKey: 'id',
+});
+db.user.hasOne(db.refreshToken, {
+  foreignKey: 'userId',
+  targetKey: 'id',
+});
+
+// end of user and refresh token relationship
+
+
 
 // start of user and roles relationship
 db.role.belongsToMany(db.user, {
