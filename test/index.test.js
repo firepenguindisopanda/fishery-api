3fd8b7a347b1ede6ea709ed8de68fd8ed161f7b0
firepenguindisopanda <nicosmith.smith3@gmail.com>
2023-01-07 22:58:26 -0400

    Updated: with skeleton template for tests

---
+++ test/index.test.js
@@ -0,0 +1,11 @@
+
+describe("Authorization Controller Testing", () => {
+    describe("Save a user to the postgresql database", () => {
+        it("should successfully add a user to the database", async () => {
+
+        });
+        it("should return a json message if a field is empty", async () => {
+
+        });
+    })
+})
